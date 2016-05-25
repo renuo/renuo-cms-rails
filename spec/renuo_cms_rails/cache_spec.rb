375d21c5acc29bb893817c7efc9f731cd3772d26
Lukas Elmer <lukas.elmer@gmail.com>
2016-05-25 19:03:41 +0200

    refactoring

--- spec/renuo_cms_rails/cache_spec.rb
+++ spec/renuo_cms_rails/cache_spec.rb
@@ -9,12 +9,16 @@ describe RenuoCmsRails::Cache do
     it 'returns the same instance two times' do
       expect(described_class.cache).to be(described_class.cache)
     end
+  end
 
+  describe '#initialize_cache' do
     it 'delegates the method call initialize_cache to the cacher' do
       expect(described_class.cache).to receive(:initialize_cache)
       described_class.initialize_cache
     end
+  end
 
+  describe '#get' do
     it 'delegates the method call get(key) to the cacher' do
       expect(described_class.cache).to receive(:get).with('content.path')
       described_class.get('content.path')
