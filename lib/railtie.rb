fae8131b195b1ff634f1321dd72292a67c5693e0
Lukas Elmer <lukas.elmer@gmail.com>
2016-05-26 13:59:44 +0200

    write initializer for the cache

--- lib/railtie.rb
+++ lib/railtie.rb
@@ -1,6 +1,18 @@
 module Railtie
   class Railtie < Rails::Railtie
-    # initializer 'setup renuo cms' do
-    # end
+    initializer 'renuo_cms_rails' do |app|
+      app.config.after_initialize do
+        require 'benchmark'
+        require 'digest/sha1'
+
+        time = Benchmark.measure do
+          Rails.logger.info 'Initializing Renuo CMS Rails Cache...'
+          cache = RenuoCmsRails::Cache.initialize_cache
+          cache_details = "found #{cache.size} contents, sha1: #{Digest::SHA1.hexdigest(cache.to_s)}"
+          Rails.logger.info "CMS cache details: #{cache_details}"
+        end
+        Rails.logger.info "Done initializing Renuo CMS Rails Cache (#{time.real.round(3)}s)"
+      end
+    end
   end
 end
--- lib/renuo-cms-rails.rb
+++ lib/renuo-cms-rails.rb
@@ -3,6 +3,7 @@ require 'renuo_cms_rails/config'
 require 'renuo_cms_rails/cms_helper'
 require 'renuo_cms_rails/api'
 require 'renuo_cms_rails/cache'
+require_relative 'railtie' if defined?(Rails::Railtie)
 
 ActiveSupport.on_load(:action_view) do
   include RenuoCmsRails::CmsHelper
